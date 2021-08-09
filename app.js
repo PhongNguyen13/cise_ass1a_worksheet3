effddc16f5d308614af14874872e6735e0ae07ba
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2021-08-09 23:11:57 +1200

    Commit

--- app.js
+++ app.js
@@ -1,61 +1,49 @@
+require("dotenv").config({path: "./config.env"});
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
-//const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
 // routes
 const books = require('./routes/api/books');
-const { Mongoose } = require('mongoose');
-
 const app = express();
 
 // Connect Database
-require("dotenv").config()
 connectDB();
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
 //import your models
-require("./models/quote");
-
-/*mongoose
-    .connectDB(
-        process.env.MONGODB_CONNECTION_STRING, 
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
-    )
-    .then(() => console.log("MongoDB has been connected"))
-    .catch((err) => console.log(err));*/
+//require("./models/quote");
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-//middleware
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-//import routes
-require("./routes/quoteRoute.js")(app);
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '/my-app/build')));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'my-app', 'build', 'index.html'))
+    });
+} else {
+    app.get('/', (req,res) => {
+        res.send('API Running');
+    });
+}
+
 const PORT = process.env.PORT || 5000;
-
-// Accessing the path module
-const path = require("path");
-
+/*
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+});*/
 
-app.listen(port, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
